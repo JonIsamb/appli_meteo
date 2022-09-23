96410e674c08d50c848e8a32f54de309deebc414
Bastien <bastien@nizart.me>
2022-09-23 14:07:25 +0200

    :construction: affichage temperature valide

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+import requests as requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    ville = "Lens"
+    meteo = requests.get("http://wttr.in/" + ville + "?format=j1")
+
+    return ville + " : " + meteo.json()["current_condition"][0]["temp_C"] + "&degC"
 
 
 if __name__ == '__main__':
