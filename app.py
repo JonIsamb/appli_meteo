c9cde1aee1771db8305ce54dde6f824fdb799677
Bastien <bastien@nizart.me>
2022-09-30 13:10:09 +0200

    mise à jour

--- app.py
+++ app.py
@@ -1,15 +1,39 @@
-from flask import Flask
+import sqlite3
+from sqlite3 import OperationalError
+
+from flask import Flask, render_template
 import requests as requests
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.secret_key = 'Ma clé secrète'
 
 
-@app.route('/')
+class Form(FlaskForm):
+    ville = StringField('ville', validators=[DataRequired()])
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    ville = "Lens"
-    meteo = requests.get("http://wttr.in/" + ville + "?format=j1")
+    form = Form()
 
-    return ville + " : " + meteo.json()["current_condition"][0]["temp_C"] + "&degC"
+    if form.validate_on_submit():
+        ville = form.ville.data
+        meteo = requests.get("http://wttr.in/" + ville + "?format=j1")
+        con = sqlite3.connect('sqlite.db', check_same_thread=False)
+        cur = con.cursor()
+
+        try:
+            cur.execute('''CREATE TABLE adresse(date text, rue text, label text, x float, y float)''')
+            con.commit()
+        except OperationalError:
+            print("Table existante !")
+
+        return ville + " : " + meteo.json()["current_condition"][0]["temp_C"] + "&degC"
+
+    return render_template('form.html', form=form)
 
 
 if __name__ == '__main__':
